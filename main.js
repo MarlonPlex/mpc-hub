fd03576f135c73a8072e88ad1aecec76348803a1
Marlon Cockett <149118438+MarlonPlex@users.noreply.github.com>
2025-07-03 18:38:38 -0500

    Complete Task 2.2
    - Music directory and .mp3 files paths retrieval.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, shell } = require('electron');
+const fs = require('node:fs');
+const path = require('node:path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -9,6 +11,28 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
+
 app.whenReady().then(() => {
   createWindow()    
+
+  const musicDirectory = app.getPath("music");
+  filteredFilePaths = [];
+  try {
+    const fileNames = fs.readdirSync(musicDirectory);
+    filePaths = fileNames.map((fileName) => path.join(musicDirectory, fileName));
+    filteredFilePaths = filePaths.filter((filePath) => fs.lstatSync(filePath).isFile());
+  } catch (error) {
+    console.error(`Error retrieving file paths from directory ${musicDirectory}`, error);    
+  }
+
+  filteredFilePaths.forEach((filePath, index, arr) => {
+    if(path.extname(filePath) !== ".mp3") {
+      arr.splice(index, 1);
+    }
+  });
+
+
+  // TODO: Task 2.3
+  console.log(filteredFilePaths);
+
 })
