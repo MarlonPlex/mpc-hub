cde0d0296ace26e741ea931afd5217e1b992d9c7
Marlon Cockett <149118438+MarlonPlex@users.noreply.github.com>
2025-07-27 22:50:32 -0500

    UI rev1 - Layout Started
    - Grid layout and panels sections initial implementation done.
    - Responsiveness to be addressed.

--- Archive/preload.js
+++ Archive/preload.js
@@ -1,6 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron/renderer');
 
-
 contextBridge.exposeInMainWorld("electronAPI", {
     onLoadTrackData: (callback) => ipcRenderer.on("tracklist-loaded", (_event, trackData) => callback(trackData))
 });
